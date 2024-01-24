42e69bace236d1cc989310d011cca0a814eb3b42
Sunil Kappal <skappal7@users.noreply.github.com>
2024-01-24 11:29:13 +0530

    Update kgraphs.py

--- kgraphs.py
+++ kgraphs.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/16FLyT_QQtDcieYhjUps6fjXaDizatNAN
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import networkx as nx
